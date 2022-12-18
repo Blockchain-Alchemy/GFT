a2b9be17cae12a4d97bbe64c382a0d33933b6fbd
Eric Thomas <bluebird6922@gmail.com>
2022-12-18 08:58:00 +0300

    update provider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,37 +1,14 @@
-import React from "react";
-import { Toaster } from "react-hot-toast";
-import { Provider as ReduxProvider } from "react-redux";
-import { CacheProvider } from "@emotion/react";
-import { StyledEngineProvider } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { createEmotionCache } from "./utils/create-emotion-cache";
-import { store } from "./store";
-import { createTheme } from "./theme";
-import { BeaconProvider } from "./contexts/BeaconContext";
+import { NetworkType } from '@airgap/beacon-sdk';
+import React, { ReactNode } from 'react';
+import { NetworkProvider } from './contexts/NetworkProvider';
+import { WalletProvider } from './contexts/WalletProvider';
 
-const clientSideEmotionCache = createEmotionCache();
-
-const Providers: React.FC<{ children?: JSX.Element | JSX.Element[] }> = ({
-  children,
-}) => (
-  <CacheProvider value={clientSideEmotionCache}>
-    <ReduxProvider store={store}>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider
-          theme={createTheme({
-            direction: "ltr",
-            responsiveFontSizes: true,
-            mode: "dark",
-          })}
-        >
-          <CssBaseline />
-          <Toaster position="top-center" />
-          <BeaconProvider>{children}</BeaconProvider>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </ReduxProvider>
-  </CacheProvider>
-);
+const Providers: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <NetworkProvider value={NetworkType.MAINNET}>
+      <WalletProvider>{children}</WalletProvider>
+    </NetworkProvider>
+  );
+};
 
 export default Providers;
--- src/contexts/WalletProvider/index.tsx
+++ src/contexts/WalletProvider/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { WalletContext, WalletProvider } from './Provider';
 
 const useWallet = () => {
