624be3a5784477398619c4bb4c161e40ab9e78a3
Eric Thomas <bluebird6922@gmail.com>
2022-12-18 09:06:57 +0300

    header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -16,7 +17,6 @@ const Header = () => {
         {/* Mobile menu button */}
         <div className="flex lg:hidden">
           <button
-            x-cloak
             type="button"
             className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
             aria-label="toggle menu"
@@ -28,11 +28,11 @@ const Header = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M4 8h16M4 16h16"
               />
             </svg>
@@ -44,11 +44,11 @@ const Header = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M6 18L18 6M6 6l12 12"
               />
             </svg>
