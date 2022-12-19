bb8cc7c4e50109a419cd6661b2ee6fd915f1bf9b
Eric Thomas <bluebird6922@gmail.com>
2022-12-19 13:21:33 +0300

    findToken

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -1,5 +1,28 @@
+import { useCallback } from 'react';
+import { useWallet } from 'contexts/WalletProvider';
+
 const useToken = () => {
-  return false;
+  const { tezos } = useWallet();
+
+  const findToken = useCallback(
+    async (address: string) => {
+      try {
+        const contract = await tezos.wallet.at('contrat_address');
+        const storage: any = await contract.storage();
+        const key = {
+          owner: address,
+          token_id: 0,
+        };
+        const operators = await storage.operators.get(key);
+        return !!operators;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [tezos]
+  );
+
+  return { findToken };
 };
 
 export default useToken;
